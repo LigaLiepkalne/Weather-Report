01cf050a0f23a3d3395b8b1aac6d67f720b3e57e
Liga Liepkalne <liepkalnes@gmail.com>
2022-11-21 11:25:08 +0200

    Small updates

--- index.php
+++ index.php
@@ -5,21 +5,20 @@ require_once "vendor/autoload.php";
 use App\WeatherData;
 use AsciiTable\Builder;
 
-$report = new WeatherData("bc888cebe0b19daf6284e6ed4bd3d141", readline("Please enter city name: "));
+$report = new WeatherData("-", readline("Please enter city name: "));
 $builder = new Builder();
 
 $builder->addRows([
   [
-    'Temperature' => round($report->getData()["main"]["temp"])."° C",
-    'Feels' => round($report->getData()["main"]["feels_like"])."° C",
-    'Max' => round($report->getData()["main"]["temp_max"])."° C",
-    'Min' => round($report->getData()["main"]["temp_min"])."° C",
+    'Temperature' => round($report->getData()["main"]["temp"])." °C",
+    'Feels like' => round($report->getData()["main"]["feels_like"])." °C",
+    'Max' => round($report->getData()["main"]["temp_max"])." °C",
+    'Min' => round($report->getData()["main"]["temp_min"])." °C",
     'Sky' => $report->getData()["weather"][0]["description"],
     'Wind speed' => round($report->getData()["wind"]["speed"])." m/s",
     'Atmospheric pressure' => $report->getData()["main"]["pressure"]." hPa",
     'Humidity' => $report->getData()["main"]["humidity"]."%",
   ]]);
-
 
 $builder->setTitle("WEATHER REPORT FOR " . strtoupper($report->getCity() . " " . date('m/d/Y h:i:s a', time())));
 
